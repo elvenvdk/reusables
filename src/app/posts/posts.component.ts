efbd9858093a484a42928875eded41d4d44c39c3
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2018-12-09 14:28:48 -0500

    Added create post method and update post method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,13 +8,32 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent implements OnInit {
   posts: any[];
-  constructor(http: Http) {
-    http
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .subscribe(response => {
-        this.posts = response.json();
-      });
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) {
+    http.get(this.url).subscribe(response => {
+      this.posts = response.json();
+      console.log(this.posts.slice(0, 1));
+    });
   }
 
   ngOnInit() {}
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
+      post['id'] = response.json();
+      this.posts.splice(0, 0, post);
+      console.log(response.json());
+    });
+  }
+
+  updatePost(post) {
+    console.log(post.id);
+    this.http
+      .patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response.json());
+      });
+  }
 }
