484cae8a940da6f349c45d7fdd28c5e0685800ab
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2018-12-09 13:59:14 -0500

    Added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { FavoriteEventArgs } from './favorite/favorite.component';
+import { likesEventArgs } from './likes/likes.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reusables';
+  post = {
+    title: 'Title',
+    isSelected: false
+  };
+  likes = {
+    count: 0,
+    isActive: false
+  };
+
+  showOnScreen: boolean;
+
+  canSave: boolean = true;
+
+  task: {} = {
+    title: 'Review Applications',
+    assignee: null
+  };
+
+  onFavoriteChange(eventArgs: FavoriteEventArgs) {
+    console.log('Favorite Changed: ', eventArgs);
+  }
+
+  onLikesChange(eventArgs: likesEventArgs) {
+    console.log('Likes Changed', eventArgs);
+    this.likes.isActive = !this.likes.isActive;
+    this.likes.isActive ? (this.likes.count = 1) : (this.likes.count = 0);
+  }
+
+  onShowOnScreen() {
+    this.showOnScreen = !this.showOnScreen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikesComponent } from './likes/likes.component';
+import { Directives1Component } from './directives1/directives1.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { NewCourseForm2Component } from './new-course-form2/new-course-form2.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoriteComponent,
+    PanelComponent,
+    LikesComponent,
+    Directives1Component,
+    InputFormatDirective,
+    ContactFormComponent,
+    SignupFormComponent,
+    NewCourseFormComponent,
+    NewCourseForm2Component,
+    PostsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
